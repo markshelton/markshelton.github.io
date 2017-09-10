83a67ee292b65a9f6ccff90ce17767f5ec33b057
Mark Shelton <mark@shelton.id.au>
2017-09-10 16:20:44 +0800

    Add scrolling navigation

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -1,7 +1,31 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".fixed-top");
-    var $header = $("#header")
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height() - $nav.height());
+$(document).ready(function(){
+
+  var $navBar = $(".fixed-top");
+  var $header = $("#header")
+  var $navItem = $('.nav-link, .navbar-brand')
+	
+	$navItem.click(function(event) { 
+		var anchorId = $(this).attr("href");
+
+    if (anchorId.length > 1 && $(anchorId).length > 0) {
+      var target = $(anchorId).offset().top - $navBar.height();
+    } else var target = 0;
+
+		$('html, body').animate({ scrollTop: target	}, 500, function () {
+			window.location.hash = anchorId;
+		});
+
   });
+  
+  $(document).scroll(function () {
+    $navBar.toggleClass('scrolled', 
+      $(this).scrollTop() > $header.height() - $navBar.height()
+    );
+  });
+
+  $('body').scrollspy({
+    target: $navBar,
+    offset: $navBar.height()
+  });
+
 });
