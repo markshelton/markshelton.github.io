9109e84e92e7561a68d25ed5a0d452a74ebe1f44
Mark Shelton <mark@shelton.id.au>
2017-11-10 11:31:03 +0800

    remove animation

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -1,35 +1,32 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
   var $navBar = $(".fixed-top");
-  var $header = $("#header")
-  var $navItem = $('.nav-link, .navbar-brand')
-	
-	$navItem.click(function(event) { 
-		var anchorId = $(this).attr("href");
+  var $header = $("#header");
+  var $navItem = $(".nav-link, .navbar-brand");
+
+  $navItem.click(function(event) {
+    var anchorId = $(this).attr("href");
 
     if (anchorId.length > 1 && $(anchorId).length > 0) {
       var target = $(anchorId).offset().top - $navBar.height();
     } else var target = 0;
 
-		$('html, body').animate({ scrollTop: target	}, 500, function () {
-			window.location.hash = anchorId;
-		});
-
+    $("html, body").animate({ scrollTop: target }, 500, function() {
+      window.location.hash = anchorId;
+    });
   });
-  
-  $(document).scroll(function () {
-    $navBar.toggleClass('scrolled', 
+
+  $(document).scroll(function() {
+    $navBar.toggleClass(
+      "scrolled",
       $(this).scrollTop() > $header.height() - $navBar.height()
     );
-
   });
 
-  $('body').scrollspy({
+  $("body").scrollspy({
     target: $navBar,
     offset: $navBar.height()
   });
-
 });
 
 window.sr = ScrollReveal();
-sr.reveal('.intro-line', { duration: 2000 }, 500);
+//sr.reveal('.intro-line', { duration: 2000 }, 500);
